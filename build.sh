e71b96ff189c3b18a62f27a343c21fc33982c7bc
Jason Pell <jason@pellcorp.com>
2019-08-18 02:47:27 +1000

    various fixes

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# only on ubuntu 18.10+
+
+sudo apt install build-essential qt5-default qttools5-dev-tools qt5-qmake qtdeclarative5-dev libqt5sql5-sqlite libqt5x11extras5-dev libmpv-dev
+
+# not actually a bzip2 :-(
+rm -rf /tmp/minitube-3.1/
+wget https://github.com/flaviotordini/minitube/releases/download/3.1/minitube-3.1.tar.bz2 -O /tmp/minitube_3.1.orig.tar.gz
+tar -zxvf /tmp/minitube_3.1.orig.tar.gz -C /tmp
+cp -r debian /tmp/minitube-3.1/
+pushd /tmp/minitube-3.1
+debuild -uc -us
+popd
+
+cp /tmp/minitube_3.1-1kidstube_amd64.deb dist/
+
---
+++ ubuntu1804-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo add-apt-repository -y -n ppa:beineri/opt-qt-5.12.0-bionic
+sudo add-apt-repository -y -n ppa:mc3man/bionic-media
+sudo apt-get update
+
+wget https://github.com/pellcorp/minitube-build/raw/master/dist/minitube_3.1-1kidstube_amd64.deb -O /tmp/minitube_3.1-1kidstube_amd64.deb
+sudo dpkg -i /tmp/minitube_3.1-1kidstube_amd64.deb
+rm /tmp/minitube_3.1-1kidstube_amd64.deb
+sudo apt-get -y install --fix-broken
+
+sudo apt-get -y install qt512-meta-minimal qt512x11extras
+
+sudo tee -a /usr/bin/kidstube <<- EOM
+#!/bin/bash
+
+source /opt/qt512/bin/qt512-env.sh
+/usr/bin/minitube $@
+EOM
+sudo chmod 777 /usr/bin/kidstube
+
+cp /usr/share/applications/minitube.desktop /tmp/minitube.desktop
+sed -i 's/=Minitube/=Kidstube/g' /tmp/minitube.desktop
+sed -i 's:Exec=minitube:Exec=kidstube:g' /tmp/minitube.desktop
+sudo desktop-file-install /tmp/minitube.desktop
+rm /tmp/minitube.desktop
+
